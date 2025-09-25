f0be36d1c64d5831d93accdbe787ec9e1dbaed02
Raghav <raghav.kaashyap@gmail.com>
2025-09-25 03:56:53 -0500

    Modified packages

--- backend/src/main/java/com/ust/backend/BackendApplication.java
+++ backend/src/main/java/com/ust/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-
+package com.ust.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/com/ust/backend/HealthController.java
+++ backend/src/main/java/com/ust/backend/HealthController.java
@@ -1,3 +1,5 @@
+package com.ust.backend;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
