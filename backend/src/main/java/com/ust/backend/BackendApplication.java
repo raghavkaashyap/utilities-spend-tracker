eef812db7f4045d690d4c521c491f7942c5c3057
Raghav <raghav.kaashyap@gmail.com>
2025-09-29 15:39:05 -0500

    Changes

--- backend/src/main/java/com/ust/backend/BackendApplication.java
+++ backend/src/main/java/com/ust/backend/BackendApplication.java
@@ -1,10 +1,12 @@
 package com.ust.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
---
+++ backend/src/main/java/com/ust/backend/model/Bill.java
@@ -0,0 +1,5 @@
+package com.ust.backend.model;
+
+public class Bill {
+
+}
