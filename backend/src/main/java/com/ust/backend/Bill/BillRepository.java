78af80bc06dfac7b80c20641b59dc1cbab4665c8
Raghav <raghav.kaashyap@gmail.com>
2025-10-01 18:31:47 -0500

    Modifications

--- backend/src/main/java/com/ust/backend/Bill/BillRepository.java
+++ backend/src/main/java/com/ust/backend/Bill/BillRepository.java
@@ -1,4 +1,8 @@
 package com.ust.backend.Bill;
 
-public class BillRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill, Long> {
 }
---
+++ backend/src/main/java/com/ust/backend/Bill/BillService.java
@@ -0,0 +1,21 @@
+package com.ust.backend.Bill;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BillService {
+
+    private final BillRepository billRepository;
+
+    @Autowired
+    public BillService(BillRepository billRepository) {
+        this.billRepository = billRepository;
+    }
+
+    public List<Bill> getBills(){
+        return billRepository.findAll();
+    }
+}
