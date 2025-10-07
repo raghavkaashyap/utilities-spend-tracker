9c0a5b706cacec537c04a3a7071c13da32efcc11
Raghav <raghav.kaashyap@gmail.com>
2025-10-07 02:48:53 -0500

    Added service methods

--- backend/src/main/java/com/ust/backend/Bill/BillService.java
+++ backend/src/main/java/com/ust/backend/Bill/BillService.java
@@ -19,10 +19,16 @@ public class BillService {
         return billRepository.findAll();
     }
 
-    public Bill getBillById(long id){
-//        if (billRepository.existsById(id)){
-//            return billRepository.findById(id);
-//        }
-        return null;
+    public List<Object[]> getSumOfAmountByUtilityType(){
+        return billRepository.getSumOfAmountByUtilityType();
     }
+
+    public List<Object[]> getSumOfAmountByStatus(){
+        return billRepository.getSumOfAmountByStatus();
+    }
+
+    public List<Object[]> getMonthlyTotals(){
+        return billRepository.getMonthlyTotals();
+    }
+
 }
