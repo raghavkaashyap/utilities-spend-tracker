e0f61761a5fd633ec4a76a665de512718839881d
Raghav <raghav.kaashyap@gmail.com>
2025-10-07 02:25:27 -0500

    Modified bill-repo

--- backend/src/main/java/com/ust/backend/Bill/BillRepository.java
+++ backend/src/main/java/com/ust/backend/Bill/BillRepository.java
@@ -3,15 +3,17 @@ package com.ust.backend.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface BillRepository extends JpaRepository<Bill, Long> {
-    Bill findById(long id);
-    List<Bill> findAll();
-    void deleteById(long id);
-    void deleteAll();
-    Bill save(Bill bill);
-    boolean existsById(long id);
 
+    List<Bill> findAllByUtilityType(UtilityType utilityType);
+    List<Bill> findAllByStatus(BillStatus status);
+    List<Bill> findAllByServiceMonthBetween(LocalDate start, LocalDate end);
+    List<Bill> findAllByUtilityType(UtilityType utilityType, Pageable pageable);
+    List<Bill> findAllByStatus(BillStatus status, Pageable pageable);
+    List<Bill> findAllByServiceMonthBetween(LocalDate start, LocalDate end, Pageable pageable);
 }
