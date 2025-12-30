2bbc9c44ac160ed9320db6da78adc7733d4359f1
Raghav <raghav.kaashyap@gmail.com>
2025-12-30 23:59:10 +0530

    JPA user details service

--- backend/src/main/java/com/ust/backend/user/JPAUserDetailsService.java
+++ backend/src/main/java/com/ust/backend/user/JPAUserDetailsService.java
@@ -1,4 +1,37 @@
 package com.ust.backend.user;
 
-public class JPAUserDetailsService {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class JPAUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepo userRepo;
+
+    public JPAUserDetailsService(AppUserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        Set<GrantedAuthority> authoritySet = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toSet());
+        return User.withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(authoritySet)
+                .accountLocked(!user.isEnabled())
+                .disabled(!user.isEnabled())
+                .build();
+    }
 }
